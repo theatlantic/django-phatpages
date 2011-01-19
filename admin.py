cd256ff70583b08ee5aba5a2a3de2b6c0706f0d4
Josh West <joshwest@atlanticmediacompany.com>
2011-01-19 17:15:05 -0500

    Added files

---
+++ admin.py
@@ -0,0 +1,26 @@
+from django import forms
+from django.contrib import admin
+from fatpages.models import FatPage
+from django.utils.translation import ugettext_lazy as _
+
+
+class FatpageForm(forms.ModelForm):
+    url = forms.RegexField(label=_("URL"), max_length=100, regex=r'^[-\w/]+$',
+        help_text = _("Example: '/about/contact/'. Make sure to have leading"
+                      " and trailing slashes."),
+        error_message = _("This value must contain only letters, numbers,"
+                          " underscores, dashes or slashes."))
+
+    class Meta:
+        model = FatPage
+
+
+class FatPageAdmin(admin.ModelAdmin):
+    form = FatpageForm
+    fieldsets = (
+        (None, {'fields': ('url', 'title', 'content')}),
+    )
+    list_display = ('title', 'url')
+    search_fields = ('title', 'url')
+
+admin.site.register(FatPage, FatPageAdmin)
---
+++ models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.sites.models import Site
+from django.utils.translation import ugettext_lazy as _
+from ckeditor.fields import RichTextField
+
+class FatPage(models.Model):
+	url = models.CharField(_('URL'), max_length=100, db_index=True)
+	title = models.CharField(_('title'), max_length=200)
+	content = RichTextField('Content', null=True, blank=True)
+	enable_comments = models.BooleanField(_('enable comments'))
+	template_name = models.CharField(_('template name'), max_length=70, blank=True,
+									 help_text=_("Example: 'fatpages/contact_page.html'. If this isn't provided, the system will use 'fatpages/default.html'."))
+
+	registration_required = models.BooleanField(_('registration required'), help_text=_("If this is checked, only logged-in users will be able to view the page."))
+
+	class Meta:
+		db_table = 'django_flatpage'
+		verbose_name = _('static page')
+		verbose_name_plural = _('static pages')
+		ordering = ('url',)
+		app_label = 'static'
+
+	def __unicode__(self):
+		return u"%s -- %s" % (self.url, self.title)
+
+	def get_absolute_url(self):
+		return self.url
