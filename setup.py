f64de87477f1beff0948fe21a74222fd9b00f561
Josh West <joshwest@atlanticmediacompany.com>
2011-01-19 18:32:34 -0500

    added setup

---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import setup, find_packages
+
+from taggit import VERSION
+
+
+f = open(os.path.join(os.path.dirname(__file__), 'README.txt'))
+readme = f.read()
+f.close()
+
+setup(
+    name='django-fatpages',
+    version=".".join(map(str, VERSION)),
+    description='django-fatpages is a reusable Django application for simple static pages.',
+    long_description=readme,
+    author='Josh West',
+    author_email='',
+    url='https://github.com/theatlantic/django-fatpages',
+    packages=find_packages(),
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+)
+
+
