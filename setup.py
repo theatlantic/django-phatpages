5fd97296b4c3b0a2fee718eebb3f8fe3890868fa
Llewellyn Hinkes <lhinkesjones@theatlantic.com>
2011-04-21 11:06:21 -0400

    Changing import in setup to static pages

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-from fatpages import VERSION
+from staticpages import VERSION
 
 
 f = open(os.path.join(os.path.dirname(__file__), 'README.txt'))
