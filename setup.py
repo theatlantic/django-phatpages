094b10d1ba16ae4458155f0c3b67deaf68d55a31
Josh West <joshwest@atlanticmediacompany.com>
2011-07-20 13:11:08 -0400

    Slight tweak to setup.py

--- setup.py
+++ setup.py
@@ -5,17 +5,12 @@ try:
     from setuptools.core import setup
 except ImportError:
     from distutils.core import setup
-import os
-
-f = open(os.path.join(os.path.dirname(__file__), 'README.txt'))
-readme = f.read()
-f.close()
 
 setup(
     name='django-fatpages',
     version='0.1',
     description='A reusable Django application for simple static pages.',
-    long_description=readme,
+    long_description=open('README.txt').read(),
     author='Josh West',
     url='https://github.com/theatlantic/django-fatpages',
     download_url='https://github.com/theatlantic/django-fatpages/tarball/master',
