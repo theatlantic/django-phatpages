ab7863b7873e3da01279e036763c8ed73bb3d4b1
Carl Johnson <cjohnson@theatlantic.com>
2013-09-24 16:46:11 -0400

    Fresh migration

---
+++ phatpages/migrations/0001_initial.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'PhatPage'
+        db.create_table('django_flatpage', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('url', self.gf('django.db.models.fields.CharField')(max_length=100, db_index=True)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=200)),
+            ('content', self.gf('ckeditor.fields.RichTextField')(null=True, blank=True)),
+            ('excerpt', self.gf('ckeditor.fields.RichTextField')(null=True, blank=True)),
+            ('enable_comments', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('template_name', self.gf('django.db.models.fields.CharField')(max_length=70, blank=True)),
+            ('registration_required', self.gf('django.db.models.fields.BooleanField')(default=False)),
+        ))
+        db.send_create_signal('phatpages', ['PhatPage'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'PhatPage'
+        db.delete_table('django_flatpage')
+
+
+    models = {
+        'phatpages.phatpage': {
+            'Meta': {'ordering': "('url',)", 'object_name': 'PhatPage', 'db_table': "'django_flatpage'"},
+            'content': ('ckeditor.fields.RichTextField', [], {'null': 'True', 'blank': 'True'}),
+            'enable_comments': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'excerpt': ('ckeditor.fields.RichTextField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'registration_required': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'template_name': ('django.db.models.fields.CharField', [], {'max_length': '70', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'url': ('django.db.models.fields.CharField', [], {'max_length': '100', 'db_index': 'True'})
+        }
+    }
+
+    complete_apps = ['phatpages']
