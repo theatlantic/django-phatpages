b91ef4fd9bd6f57f7bed470830f4cdd117e21235
Frankie Dintino <fdintino@theatlantic.com>
2016-01-29 14:54:49 -0500

    From version from __init__.py to setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
 
 setup(
     name='django-phatpages',
-    version=__import__('phatpages').__version__,
+    version='2.0.2',
     description='A reusable Django application for simple static pages.',
     long_description=open('README.txt').read(),
     author='Josh West',
